e97360340abc768251ed16f31a1fe343e8d2a53d
Mustafa KURAL <mustafakural@outlook.com>
2024-03-23 10:51:36 +0300

    DbServise Urunler işlemleri eklendi eklendi

--- BtkProje.Servis/DbServisi.cs
+++ BtkProje.Servis/DbServisi.cs
@@ -15,26 +15,6 @@ namespace BtkProje.Servis
         static BtkProjeDbContext ctx = new BtkProjeDbContext();
         public static TblKullanici OturumAcanKullanici { get; private set; }
 
-        public static void Kaydet()
-        {
-            ctx.SaveChanges();
-        }
-
-        public static List<TblKullanici> KullaniciListesi()
-        {
-            return ctx.Kullanicilar.ToList();
-        }
-        public static BindingList<TblKullanici> KullaniciBagliListesi()
-        {
-            ctx.Kullanicilar.Load();
-            return ctx.Kullanicilar.Local.ToBindingList();
-        }
-
-        public static void KullaniciSil(TblKullanici kullanici)
-        {
-            ctx.Kullanicilar.Remove(kullanici);
-        }
-
         public static bool OturumAc(string kullaniciAdi, string parola)
         {
             TblKullanici kull = ctx.Kullanicilar.
@@ -49,5 +29,89 @@ namespace BtkProje.Servis
             else
                 return false;
         }
+
+        public static void Kaydet()
+        {
+            ctx.SaveChanges();
+        }
+        #region Kullanici İşlemleri
+        public static List<TblKullanici> KullaniciListesi()
+        {
+            return ctx.Kullanicilar.ToList();
+        }
+        public static BindingList<TblKullanici> KullaniciBagliListesi()
+        {
+            ctx.Kullanicilar.Load();
+            return ctx.Kullanicilar.Local.ToBindingList();
+        }
+
+        public static void KullaniciSil(TblKullanici kullanici)
+        {
+            ctx.Kullanicilar.Remove(kullanici);
+        }
+        #endregion
+
+        #region Kategori İşlmeleri
+        public static List<TblKategori> KategoriListesi()
+        {
+            return ctx.Kategoriler.Include(x=>x.Urunler).ToList();
+        }
+
+        public static BindingList<TblKategori> KategoriBagliListesi()
+        {
+            ctx.Kategoriler.Include(x=>x.Urunler).Load();
+            return ctx.Kategoriler.Local.ToBindingList();
+        }
+        public static void KategoriEkle(TblKategori kategori)
+        {
+            ctx.Kategoriler.Add(kategori);
+        }
+        public static void KategoriSil(TblKategori kategori)
+        {
+            ctx.Kategoriler.Remove(kategori);
+        }
+        #endregion
+
+        #region Birim İşlemleri
+        public static List<TblBirim> BirimListesi()
+        {
+            return ctx.Birimler.ToList();
+        }
+
+        public static BindingList<TblBirim> BirimBagliListesi()
+        {
+            ctx.Birimler.Load();
+            return ctx.Birimler.Local.ToBindingList();
+        }
+        public static void BirimiEkle(TblBirim birim)
+        {
+            ctx.Birimler.Add(birim);
+        }
+        public static void BirimSil(TblBirim birim)
+        {
+            ctx.Birimler.Remove(birim);
+        }
+        #endregion
+
+        #region Ürün İşlemleri
+        public static List<TblUrun> UrunListesi()
+        {
+            return ctx.Urunler.ToList();
+        }
+
+        public static BindingList<TblUrun> UrunBagliListesi()
+        {
+            ctx.Urunler.Load();
+            return ctx.Urunler.Local.ToBindingList();
+        }
+        public static void UrunEkle(TblUrun urun)
+        {
+            ctx.Urunler.Add(urun);
+        }
+        public static void UrunSil(TblUrun urun)
+        {
+            ctx.Urunler.Remove(urun);
+        }
+        #endregion
     }
 }
